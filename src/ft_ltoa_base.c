bf5ffec937a05deae3382eadaa850e2c13084953
pdlmn <jseldardw@gmail.com>
2023-11-22 23:43:57 +0100

    feat: added length calculations for csdip flags

---
+++ src/ft_ltoa_base.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ltoa_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/20 12:20:15 by emuminov          #+#    #+#             */
+/*   Updated: 2023/11/22 16:09:38 by emuminov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../lib/libft.h"
+
+static int	ft_count_digits_base_l(long n, size_t base_l)
+{
+	long long	nbr;
+	int			d;
+
+	nbr = n;
+	if (nbr < 0)
+		nbr = -nbr;
+	d = (nbr == 0);
+	while (nbr > 0)
+	{
+		nbr /= base_l;
+		d++;
+	}
+	return (d);
+}
+
+char	*ft_ltoa_base(const char *base, long n)
+{
+	int			d;
+	long long	nbr;
+	size_t		base_l;
+	short		is_neg;
+	char		*str;
+
+	base_l = ft_strlen(base);
+	if (!base_l)
+		return (0);
+	d = ft_count_digits_base_l(n, base_l);
+	is_neg = (n < 0);
+	str = malloc(sizeof(char) * (d + is_neg + 1));
+	if (!str)
+		return (0);
+	if (is_neg)
+		str[0] = '-';
+	nbr = n * ((1 * (is_neg == 0)) + (-1 * (is_neg == 1)));
+	str[d + is_neg] = '\0';
+	while (d--)
+	{
+		str[d + is_neg] = base[(nbr % base_l)];
+		nbr /= base_l;
+	}
+	return (str);
+}
--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 21:32:35 by emuminov          #+#    #+#             */
-/*   Updated: 2023/11/21 23:14:02 by emuminov         ###   ########.fr       */
+/*   Updated: 2023/11/22 23:40:51 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,56 +36,131 @@ static void	ft_putnbr_unsigned_fd(unsigned int n, int fd)
 	}
 }
 
-static void ft_puthex(va_list args)
+static int ft_puthex_ptr(va_list args)
 {
-	void *arg = va_arg(args, void *);
+	size_t	l;
+	void	*arg;
+
+	arg = va_arg(args, void *);
 	if (!arg)
 	{
 		ft_putstr_fd("(nil)", 0);
-		return ;
+		return (5);
 	}
+	char *hex = ft_ltoa_base("0123456789abcdef", (long) arg);
+	if (!hex)
+	{
+		ft_putstr_fd("(nil)", 0);
+		return (5);
+	}
+	ft_putstr_fd("0x", 0);
+	ft_putstr_fd(hex, 0);
+	l = ft_strlen(hex);
+	free(hex);
+	return (l + 2);
+}
+
+static void ft_puthex_ui(int toupper, va_list args)
+{
+	size_t	i;
+	unsigned int arg = va_arg(args, unsigned int);
 	char *hex = ft_ltoa_base("0123456789abcdef", (long) arg);
 	if (!hex)
 	{
 		ft_putstr_fd("(nil)", 0);
 		return ;
 	}
-	ft_putstr_fd("0x", 0);
+	if (toupper)
+	{
+		i = 0;
+		while (hex[i])
+		{
+			hex[i] = ft_toupper(hex[i]);
+			i++;
+		}
+	}
 	ft_putstr_fd(hex, 0);
 	free(hex);
 }
 
-void	ft_format(char flag, va_list args)
+static int	ft_putstr_arg(va_list args)
 {
+	size_t		l;
+	const char	*str;
+
+	str = va_arg(args, const char *);
+	l = ft_strlen(str);
+	write(0, str, l);
+	return (l);
+}
+
+static int	ft_putchar_arg(va_list args)
+{
+	ft_putchar_fd(va_arg(args, int), 0);
+	return (1);
+}
+
+static int	ft_putnbr_arg(va_list args)
+{
+	char	*str;
+	size_t	l;
+
+	str = ft_itoa(va_arg(args, int));
+	if (!str)
+		return (0);
+	l = ft_strlen(str);
+	write(0, str, l);
+	free(str);
+	return (l);
+}
+
+int	ft_format(char flag, va_list args)
+{
+	int	count;
+
+	count = 1;
 	if (flag == 'c')
-		ft_putchar_fd(va_arg(args, int), 0);
+		count = ft_putchar_arg(args);
 	else if (flag == 's')
-		ft_putstr_fd(va_arg(args, char *), 0);
+		count = ft_putstr_arg(args);
 	else if (flag == 'd' || flag == 'i')
-		ft_putnbr_fd(va_arg(args, int), 0);
+		count = ft_putnbr_arg(args);
 	else if (flag == 'u')
-		ft_putnbr_unsigned_fd(va_arg(args, unsigned int), 0);
+		count = ft_putnbr_unsigned_fd(va_arg(args, unsigned int), 0);
 	else if (flag == 'p')
-		ft_puthex(args);
+		count = ft_puthex_ptr(args);
+	else if (flag == 'x')
+		ft_puthex_ui(0, args);
+	else if (flag == 'X')
+		ft_puthex_ui(1, args);
+	else if (flag == '%')
+		ft_putchar_fd('%', 0);
+	return (count);
 }
 
 int		ft_printf(const char *str, ...)
 {
 	va_list	args;
-	int		i;
+	size_t	i;
+	size_t	count;
 
-	va_start(args, str); i = 0;
+	va_start(args, str);
+	i = 0;
+	count = 0;
 	while (str[i])
 	{
 		if (str[i] == '%')
 		{
-			ft_format(str[i + 1], args);
+			count += ft_format(str[i + 1], args);
 			i++;
 		}
 		else
+		{
 			ft_putchar_fd(str[i], 0);
+			count++;
+		}
 		i++;
 	}
 	va_end(args);
-	return (0);
+	return (count);
 }
